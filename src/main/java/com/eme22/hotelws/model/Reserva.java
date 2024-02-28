2483106607458a99028f0632c74d44087351dc3e
eme22 <48258734+eme22@users.noreply.github.com>
2024-02-28 14:09:16 -0500

    Updated reservas

--- src/main/java/com/eme22/hotelws/model/Reserva.java
+++ src/main/java/com/eme22/hotelws/model/Reserva.java
@@ -24,7 +24,7 @@ public class Reserva {
     @GeneratedValue
     @JdbcType(VarcharJdbcType.class)
     private UUID id;
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id")
     @JsonIgnore
     @ToString.Exclude
